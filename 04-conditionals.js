77c6875e117235964d9d03c37a68e06321d0d706
Ty Carroll <typrograms.238@gmail.com>
2020-12-24 17:42:52 -0500

    1st commit

---
+++ 04-conditionals.js
@@ -0,0 +1,76 @@
+// Practice with a single number
+const num = 9;
+
+/*
+1. Only print this message if num is greater than 10.
+Try changing the value of num to make sure your code works.
+*/
+if (num > 10) {
+  console.log('num is greater than 10');
+}
+
+
+/*
+2. Only print the message 'num is less than 100' when appropriate.
+*/
+
+
+/*
+3. Now do the same with 'num is a positive number'.
+*/
+
+
+/*
+4. 'num is between 10 and 100' (exclusive)
+*/
+
+
+/*
+5. 'num is even'
+*/
+
+
+/*
+6. 'num is an even number between 20 and 30' (inclusive)
+*/
+
+
+/*
+7. 'num is odd or negative'
+*/
+
+
+// Practice comparing numbers
+const x = 3;
+const y = 5;
+
+// console.log('The bigger number is:');
+/*
+8. Print either x or y, whichever is bigger.
+*/
+
+
+/*
+9. Print either message 'x and y are equal' or 'x and y are not equal'.
+*/
+
+
+// Let's add another number into the mix.
+const z = 2;
+
+// console.log('The biggest number is:');
+/*
+10. Print either x, y, or z, whichever is bigger.
+Remember to change the values of x, y, and z to make sure it works!
+*/
+
+
+// Practice with more operators
+const walletInCents = 25;
+const priceInDollars = 1.25;
+
+/*
+11. Print 'Exact amount!' when the wallet contains the exact amount for the price. Print 'More than enough.' when there is more than enough money in the wallet. Print 'Not enough money.' when there is not enough money in the wallet.
+
+Notice that the wallet is in cents and the price is in dollars, so you'll have to do a bit of work to compare them.
+*/
