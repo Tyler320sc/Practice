2307f3f0fb681d7ac5cc1f0bf4ff31133749a4f0
Ty Carroll <typrograms.238@gmail.com>
2020-12-31 17:47:31 -0500

    finished problem 15

--- 04-conditionals.js
+++ 04-conditionals.js
@@ -122,7 +122,7 @@ Notice that the wallet is in cents and the price is in dollars, so you'll have t
 /*
 12. Do the same thing as in 11, but let's add in some error handling. In the case where the wallet is negative, print 'Wallet cannot be negative.' When the price is negative, print 'Price cannot be negative.' Think about the importance of the order of the conditions.
 */
-// const walletInCents = 1;
+// const walletInCents = 99;
 // const priceInDollars = 1;
 // const priceInCents = priceInDollars * 100;
 
@@ -141,22 +141,23 @@ Notice that the wallet is in cents and the price is in dollars, so you'll have t
 // Practice comparing strings
 // let username = 'Alice';
 // let password = '1234';
-// const expectedUsername = 'Zed';
-// const expectedPassword = '9876';
+// const expectedUsername = 'Alice';
+// const expectedPassword = '1234';
 
 // if (username !== expectedUsername) {
-//   console.log('Invalid Login!');
+//   console.log('Invalid username!');
 // } else if (password !== expectedPassword) {
-//   console.log('Invalid Login!');
+//   console.log('Invalid password!');
 // } else {
 //   console.log('Welcome!');
 // }
+
 /*
 13. To simulate authentication, we have a username, a password, and expected values for each. The login is successful if the username and password match the expected values. Otherwise, either the username is wrong or the password is wrong. Print appropriate messages for each of the 3 cases. (Don't worry about the case where the username and password are both wrong; one of the other cases will catch it.)
 */
 // Practice nesting conditionals
-// const a = 0;
-// const b = 0;
+// const a = -1;
+// const b = -2;
 /*
 14. Print the appropriate message for any of the following cases comparing a and b:
 Matching positive numbers.
@@ -167,25 +168,58 @@ No match.
 */
 // if (a === b && a > -1 && b > -1) {
 //   console.log('Matching positive numbers.');
-// }
-// if (a === b && a < 0 && b < 0) {
+// } else if (a === b && a < 0 && b < 0) {
 //   console.log('Matching negative numbers.');
-// }
-// if (a !== b && a > -1 && b > -1) {
+// } else if (a !== b && a > -1 && b > -1) {
 //   console.log('No match of positive numbers.');
-// }
-// if (a !== b && a < 0 && b < 0) {
+// } else if (a !== b && a < 0 && b < 0) {
 //   console.log('No match of negative numbers.');
-// }
-// if (a !== b) {
+// } else {
 //   console.log('No match.');
 // }
+// const a = 'ppp';
+// const b = 110;
+
+// if (a === b) {
+//   if (a > -1 && b > -1) {
+//     console.log('Matching positive numbers.');
+//   } else {
+//     console.log('Matching negative numbers.');
+//   } 
+// } else {
+//   if (a < 0 && b < 0) {
+//     console.log('No match of negative numbers.');
+//   } else if (a > -1 && b > -1) {
+//     console.log('No match of positive numbers.');
+//   } else {
+//     console.log('No match.');
+//   }
+// }
+// const a = 1;
+// const b = 110;
+
+// if (a > -1 && b > -1) {
+//   if (a === b) {
+//     console.log('Matching positive numbers.');
+//   } else {
+//     console.log('No match of positive numbers.');
+//   }
+// } else {
+//   if (a !== b) {
+//     console.log('No match of negative numbers.');
+//   } else if (a < 0 && b < 0) {
+//     console.log('Matching negative numbers.');
+//   } else {
+//     console.log('No match.'); // Almost seems like else is not nessesary
+//   }
+// }
+
 // Practice with more information
-const myUsername = 'Alice';
-const myPassword = '1234';
-const salary = 25000;
-const employeeUsername = 'Zarya';
-const employeePassword = '9876';
+const myUsername = 'Xyrho';
+const myPassword = 'hunter2';
+const salary = 11000;
+const employeeUsername = 'Alice';
+const employeePassword = '1234';
 const ceoUsername = 'Xyrho';
 const ceoPassword = 'hunter2';
 const juniorSalaryMinimum = 50000;
@@ -196,16 +230,41 @@ const seniorSalaryMinimum = 95000;
 const seniorSalaryMaximum = 120000;
 const ceoSalaryMinimum = 110000;
 
+// if (myUsername === employeeUsername && myPassword === employeePassword) {
+//   if (salary >= juniorSalaryMinimum && salary <= juniorSalaryMaximum) {
+//     console.log(`Welcome, junior employee ${myUsername}.`);
+//   } else if (salary >= intermediateSalaryMinimum && salary <= intermediateSalaryMaximum) {
+//     console.log(`Welcome, intermediate employee ${myUsername}.`);
+//   } else if (salary >= seniorSalaryMinimum && salary <= seniorSalaryMaximum) {
+//     console.log(`Welcome, senior employee ${myUsername}.`);
+//   } else {
+//     console.log(`Welcome, employee ${myUsername}.`);
+//   }
+// } else if (myUsername === ceoUsername && myPassword === ceoPassword) {
+//   if (salary >= ceoSalaryMinimum) {
+//     console.log(`Welcome CEO.`);
+//   } else {
+//     console.log('Imposter!'); 
+//   }
+// } else {
+//   console.log(`Invalid Login.`);
+// }
 /*
 15. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in. For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.' In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.' If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter! For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
 If you notice you're repeating a certain condition more than once, try nesting if statements to avoid the repeitition.
 */
 // Practice complex conditions
-const creditCard = 123;
-// process.stdout.write('\n16. ');
+const creditCard = 22222;
+
 /*
 16. Print a message saying whether the credit card number is valid or not. A valid credit card in this system must have all of the following properties:
+
 - It is exactly 5 digits
 - Tripling it gives an even number
 - It is divisible by either 5 or 7
 */
+// if ((`${creditCard}`.length === 5) && ((creditCard * 3) % 2 === 0) && (creditCard / 5 === 0) || creditCard / 7 === 0) {
+//   console.log('Credit card is valid.');
+// } else {
+//   console.log('Credit card is invalid.');
+// }
