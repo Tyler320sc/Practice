d02912aefc8686ba05f2f690a5d71371a0197438
Ty Carroll <typrograms.238@gmail.com>
2021-02-11 23:19:47 -0500

    working on 7.45 and 7.46 / forgot to save my solution before commiting a min ago

--- 07-arrays-exercises.js
+++ 07-arrays-exercises.js
@@ -351,7 +351,7 @@ console.log(middle([1, 2, 3, 4, 5]) === 3);
 console.log(middle([1, 2, 3, 4, 5, 6, 7]) === 4);
 console.log(middle([true, 'Hello!', false]) === 'Hello!');
 console.log(middle([1]) === 1);
-console.log(middle([1, 2]) === 2);                         // These may already pass
+console.log(middle([1, 2]) === 2);                         
 console.log(middle([1, 2, 3]) === 2);
 console.log(middle([1, 2, 3, 4]) === 3);
 console.log(middle([1, 2, 3, 4, 5]) === 3);
