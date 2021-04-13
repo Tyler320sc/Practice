a9dde3937bb889f48ac8d6f83823645d4e18a7b3
Ty Carroll <typrograms.238@gmail.com>
2021-04-12 21:30:26 -0400

    working on 8.65

--- 04-conditionals.js
+++ 04-conditionals.js
@@ -195,24 +195,24 @@ No match.
 //     console.log('No match.');
 //   }
 // }
-// const a = 1;
-// const b = 110;
+const a = 1;
+const b = 110;
 
-// if (a > -1 && b > -1) {
-//   if (a === b) {
-//     console.log('Matching positive numbers.');
-//   } else {
-//     console.log('No match of positive numbers.');
-//   }
-// } else {
-//   if (a !== b) {
-//     console.log('No match of negative numbers.');
-//   } else if (a < 0 && b < 0) {
-//     console.log('Matching negative numbers.');
-//   } else {
-//     console.log('No match.'); // Almost seems like else is not nessesary
-//   }
-// }
+if (a > -1 && b > -1) {
+  if (a === b) {
+    console.log('Matching positive numbers.');
+  } else {
+    console.log('No match of positive numbers.');
+  }
+} else {
+  if (a !== b) {
+    console.log('No match of negative numbers.');
+  } else if (a < 0 && b < 0) {
+    console.log('Matching negative numbers.');
+  } else {
+    console.log('No match.'); // Almost seems like else is not nessesary
+  }
+}
 
 // Practice with more information
 const myUsername = 'Xyrho';
@@ -230,25 +230,25 @@ const seniorSalaryMinimum = 95000;
 const seniorSalaryMaximum = 120000;
 const ceoSalaryMinimum = 110000;
 
-// if (myUsername === employeeUsername && myPassword === employeePassword) {
-//   if (salary >= juniorSalaryMinimum && salary <= juniorSalaryMaximum) {
-//     console.log(`Welcome, junior employee ${myUsername}.`);
-//   } else if (salary >= intermediateSalaryMinimum && salary <= intermediateSalaryMaximum) {
-//     console.log(`Welcome, intermediate employee ${myUsername}.`);
-//   } else if (salary >= seniorSalaryMinimum && salary <= seniorSalaryMaximum) {
-//     console.log(`Welcome, senior employee ${myUsername}.`);
-//   } else {
-//     console.log(`Welcome, employee ${myUsername}.`);
-//   }
-// } else if (myUsername === ceoUsername && myPassword === ceoPassword) {
-//   if (salary >= ceoSalaryMinimum) {
-//     console.log(`Welcome CEO.`);
-//   } else {
-//     console.log('Imposter!'); 
-//   }
-// } else {
-//   console.log(`Invalid Login.`);
-// }
+if (myUsername === employeeUsername && myPassword === employeePassword) {
+  if (salary >= juniorSalaryMinimum && salary <= juniorSalaryMaximum) {
+    console.log(`Welcome, junior employee ${myUsername}.`);
+  } else if (salary >= intermediateSalaryMinimum && salary <= intermediateSalaryMaximum) {
+    console.log(`Welcome, intermediate employee ${myUsername}.`);
+  } else if (salary >= seniorSalaryMinimum && salary <= seniorSalaryMaximum) {
+    console.log(`Welcome, senior employee ${myUsername}.`);
+  } else {
+    console.log(`Welcome, employee ${myUsername}.`);
+  }
+} else if (myUsername === ceoUsername && myPassword === ceoPassword) {
+  if (salary >= ceoSalaryMinimum) {
+    console.log(`Welcome CEO.`);
+  } else {
+    console.log('Imposter!'); 
+  }
+} else {
+  console.log(`Invalid Login.`);
+}
 /*
 15. We have another authentication, but this time with a salary as well. We want to print a personalized welcome message to users who log in. For example, if the username and password match the employee username and password and their salary is within the junior range, print 'Welcome, junior employee Zarya.' In the case where the salary is not within any of the set ranges, simply print 'Welcome, employee Zarya.' If the username and password match the CEO credentials but the salary is below the CEO's minimum salary, we have an imposter! For an invalid login, just print 'Invalid login.' (don't worry about whether it was the username or password that didn't match).
 If you notice you're repeating a certain condition more than once, try nesting if statements to avoid the repeitition.
